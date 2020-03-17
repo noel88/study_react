6b15c9269ca37e153c967e7c222eb33b1cdc51f4
xiah <xiah@rtruesoft.kr>
2020-03-17 10:30:01 +0900

    udemy - 24. Inline Styles & StyleSheet Objects

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -3,11 +3,11 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{padding: 100}}>
-     <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+    <View style={styles.screen}>
+     <View style={styles.inputContainer}>
        <TextInput
            placeholder="Course Goal"
-           style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
+           style={styles.input}
        />
        <Button title="ADD" />
      </View>
@@ -19,5 +19,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    padding: 50
+  },
+  inputContainer : {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  },
 });
