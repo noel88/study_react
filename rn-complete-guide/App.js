5bc035f41c1256f47d03ce962d56786f5e5bcac2
xiah <xiah@rtruesoft.kr>
2020-03-17 10:41:45 +0900

    udemy - 25. Working with State & Events

--- rn-complete-guide/App.js
+++ rn-complete-guide/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal]);
+  };
+
   return (
     <View style={styles.screen}>
      <View style={styles.inputContainer}>
        <TextInput
            placeholder="Course Goal"
            style={styles.input}
+           onChangeText={goalInputHandler}
+           value={enteredGoal}
        />
-       <Button title="ADD" />
+       <Button
+           title="ADD"
+           onPress={addGoalHandler}/>
      </View>
       <View>
 
