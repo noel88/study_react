ef7d79038dc5484c1b7918ec416d412e6f237ddf
xiah <xiah@rtruesoft.kr>
2020-03-18 10:41:15 +0900

    udemy - 55. Styling a View as a "Card" Container

--- rn-complete-guide1/screens/StartGameScreen.js
+++ rn-complete-guide1/screens/StartGameScreen.js
@@ -40,7 +40,15 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center'
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        elevation: 8,
+        backgroundColor: 'white',
+        padding: 20,
+        borderRadius: 10
     },
     buttonContainer: {
         flexDirection: 'row',
