d0df32bcc35c14bfd1f9bee97755cca5bef3d999
xiah <xiah@rtruesoft.kr>
2020-03-18 10:15:13 +0900

    udemy - 52. Adding a Custom Header Component

--- rn-complete-guide1/App.js
+++ rn-complete-guide1/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <Header title="Guess a Number"/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  screen: {
+    flex: 1
+  }
 });
