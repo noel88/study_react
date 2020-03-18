6f44c9a4e24fa00f25c27224f70bc41a2831de12
xiah <xiah@rtruesoft.kr>
2020-03-18 11:28:15 +0900

    udemy - 60. Cleaning User Input & Controlling the Soft Keyboard

--- rn-complete-guide1/screens/StartGameScreen.js
+++ rn-complete-guide1/screens/StartGameScreen.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     View,
     Text,
     StyleSheet,
     TextInput,
-    Button
+    Button,
+    TouchableWithoutFeedback,
+    Keyboard
 } from 'react-native';
 import Card from "../components/Card";
 import Colors from "../constants/colors";
@@ -13,29 +15,42 @@ import Input from "../components/Input";
 
 
 const StartGameScreens = props => {
+
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const numberInputHandler = inputText => {
+        setEnteredValue(inputText.replace(/[^0-9]/g), '');
+    };
+
     return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start a New Game!</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Select a Number</Text>
-                <Input
-                    style ={styles.input}
-                    blurOnSubmit
-                    autoCapitalize='none'
-                    autoCorrenct={false}
-                    keyboardType="numeric"
-                    maxLength={2}
-                />
-                <View style={styles.buttonContainer}>
-                    <View style={styles.button}>
-                        <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+        }}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start a New Game!</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select a Number</Text>
+                    <Input
+                        style ={styles.input}
+                        blurOnSubmit
+                        autoCapitalize='none'
+                        autoCorrenct={false}
+                        keyboardType="numeric"
+                        maxLength={2}
+                        onChangeText={numberInputHandler}
+                        value={enteredValue}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.button}>
+                            <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+                        </View>
+                        <View style={styles.button}>
+                            <Button title="Confirm" onPress={() => {}} color={Colors.primary} />
+                        </View>
                     </View>
-                    <View style={styles.button}>
-                        <Button title="Confirm" onPress={() => {}} color={Colors.primary} />
-                    </View>
-                </View>
-            </Card>
-        </View>
+                </Card>
+            </View>
+        </TouchableWithoutFeedback>
 
     );
 };
