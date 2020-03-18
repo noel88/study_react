f02e0cc94fdedd01e2ef9f234afa23aca86bc966
xiah <xiah@rtruesoft.kr>
2020-03-18 10:31:41 +0900

    udemy - 54. Working on the Layout

--- rn-complete-guide1/screens/StartGameScreen.js
+++ rn-complete-guide1/screens/StartGameScreen.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    TextInput,
+    Button
 } from 'react-native';
 
 
@@ -10,8 +12,17 @@ import {
 const StartGameScreens = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Game Screen</Text>
+            <Text style={styles.title}>Start a New Game!</Text>
+            <View style={styles.inputContainer}>
+                <Text>Select a Number</Text>
+                <TextInput/>
+                <View style={styles.buttonContainer}>
+                    <Button title="Reset" onPress={() => {}}/>
+                    <Button title="Confirm" onPress={() => {}} />
+                </View>
+            </View>
         </View>
+
     );
 };
 
@@ -21,6 +32,21 @@ const styles = StyleSheet.create({
         padding: 10,
         alignItems: 'center',
         justifyContent: 'flex-start'
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15
     }
 });
 
