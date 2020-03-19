32b80e2f23028e0ab0f381ca23d5a648c93e12f8
xiah <xiah0526@gmail.com>
2020-03-19 10:23:50 +0900

    Nomad (React Hooks) - #1.3 useTabs

--- react-hooks/src/index.js
+++ react-hooks/src/index.js
@@ -2,32 +2,37 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const useInput = (initialValue, validator) => {
-    const [value, setValue] = useState(initialValue);
-    const onChange = (event) => {
-        const {
-            target: {value}
-        } = event;
-        let willUpdate = true;
-        if (typeof  validator === "function") {
-            willUpdate = validator(value);
-        }
-        if (willUpdate) {
-            setValue(value);
-        }
 
+const content = [
+    {
+        tab: "Section 1",
+        content: "I'm the content of the Section 1"
+    },
+    {
+        tab: "Section 2",
+        content: "I'm the content of the Section 2"
+    },
+];
+
+const useTabs = (initialTabs, allTabs) => {
+    const [currentIndex, setCurrentIndex] = useState(initialTabs);
+    if(!allTabs || !Array.isArray(allTabs)) {
+        return;
+    }
+    return {
+        currentItem: allTabs[currentIndex],
+        changeItem: setCurrentIndex
     };
-    return {value, onChange};
 };
 
-
 const App = () => {
-    const maxLen = (value) => !value.includes('@');
-    const name = useInput("Mr.", maxLen);
+    const {currentItem, changeItem} = useTabs(0, content);
     return (
         <div className="App">
-            <h1>Hello </h1>
-            <input placeholder="Name" {...name}/>
+            {content.map((section, index) => (
+                <button onClick={() => changeItem(index)}>{section.tab}</button>
+            ))}
+            {currentItem.content}
         </div>
     )
 };
