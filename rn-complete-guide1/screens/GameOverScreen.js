16dcc4fa85926e6f3ceba6a3f4167e482d6d4c7b
xiah <xiah0526@gmail.com>
2020-03-19 13:59:19 +0900

    udemy - 73. Styling Images

--- rn-complete-guide1/screens/GameOverScreen.js
+++ rn-complete-guide1/screens/GameOverScreen.js
@@ -14,7 +14,13 @@ const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
             <TitleText>The Game is over!</TitleText>
-            <Image source={require('../assets/success.png')}/>
+            <View style={styles.imageContainer}>
+                <Image
+                    source={require('../assets/success.png')}
+                    style={styles.image}
+                    resizeMode="cover"
+                />
+            </View>
             <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
             <BodyText>Number was: {props.userNumber}</BodyText>
             <Button title="NEW GAME" onPress={props.onRestart}/>
@@ -27,6 +33,18 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    imageContainer: {
+        width: 300,
+        height: 300,
+        borderRadius: 200,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden'
+    },
+    image: {
+        width: '100%',
+        height: '100%'
     }
 });
 
