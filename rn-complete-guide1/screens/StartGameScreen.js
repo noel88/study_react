755d45e52de3f545a7174aa4aad9bf75b0d6dcfb
xiah <xiah0526@gmail.com>
2020-03-19 16:18:50 +0900

    udemy - 89. Introducing the Dimensions API

--- rn-complete-guide1/screens/StartGameScreen.js
+++ rn-complete-guide1/screens/StartGameScreen.js
@@ -7,7 +7,8 @@ import {
     Button,
     TouchableWithoutFeedback,
     Keyboard,
-    Alert
+    Alert,
+    Dimensions
 } from 'react-native';
 import Card from "../components/Card";
 import Colors from "../constants/colors";
@@ -116,7 +117,8 @@ const styles = StyleSheet.create({
         paddingHorizontal: 15
     },
     button: {
-        width: 100
+        // width: 100
+        width: Dimensions.get('window').width / 4
     },
     input: {
         width: 50,
