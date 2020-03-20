85cbba12dfe10fba0ffdb4a6404584992e22f471
xiah <xiah0526@gmail.com>
2020-03-20 17:08:53 +0900

    udemy - 119. Passing & Reading Params Upon Navigation

--- the-meals-app/screens/CategoriesScreen.js
+++ the-meals-app/screens/CategoriesScreen.js
@@ -17,7 +17,10 @@ const CategoriesScreen = props => {
                 style={styles.gridItem}
                 onPress={() => {
                     props.navigation.navigate({
-                        routeName: 'CategoryMeals'
+                        routeName: 'CategoryMeals',
+                        params: {
+                            categoryId: itemData.item.id
+                        }
                     });
             }}>
                 <View>
--- the-meals-app/screens/CategoryMealScreen.js
+++ the-meals-app/screens/CategoryMealScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import {CATEGORIES} from "../data/dummy-data";
 
 const CategoryMealScreen = props => {
+    const catId = props.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
     return (
         <View style={styles.screen}>
             <Text> this is Category Meal Screen! </Text>
+            <Text> {selectedCategory.title} </Text>
             <Button title="Go to Details" onPress={() => {
                 props.navigation.navigate({
                     routeName: 'MealDetail'
