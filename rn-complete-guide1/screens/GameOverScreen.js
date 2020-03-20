9d98b61f310428d57d42419c246c67f1a486ce6c
xiah <xiah0526@gmail.com>
2020-03-20 10:25:19 +0900

    udemy - 96. Fixing the GameOver Screen

--- rn-complete-guide1/screens/GameOverScreen.js
+++ rn-complete-guide1/screens/GameOverScreen.js
@@ -49,7 +49,8 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        paddingVertical: 10
     },
     imageContainer: {
         width: Dimensions.get('window').width * 0.7,
