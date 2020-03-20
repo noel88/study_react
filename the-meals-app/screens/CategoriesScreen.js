23e1612a9061d10327814b70b0da84c240595b71
xiah <xiah0526@gmail.com>
2020-03-20 15:00:15 +0900

    udemy - 113. Navigating Between Screens

--- the-meals-app/screens/CategoriesScreen.js
+++ the-meals-app/screens/CategoriesScreen.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import MealsNavigator from "../navigation/MealsNavigator";
 
 const CategoriesScreen = props => {
     return (
         <View style={styles.screen}>
             <Text> this is Categories Screen! </Text>
+            <Button title="Go to Meals!" onPress={() => {
+                props.navigation.navigate({
+                    routeName: 'CategoryMeals'
+                });
+            }} />
         </View>
     )
 };
