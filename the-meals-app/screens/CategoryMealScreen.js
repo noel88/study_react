a5eb8ba2d2bbf174509725526ccd85809715c0aa
xiah <xiah0526@gmail.com>
2020-03-20 15:04:12 +0900

    udemy - 115. Navigation to the "Meal Details" Screen

--- the-meals-app/screens/CategoryMealScreen.js
+++ the-meals-app/screens/CategoryMealScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 const CategoryMealScreen = props => {
     return (
         <View style={styles.screen}>
             <Text> this is Category Meal Screen! </Text>
+            <Button title="Go to Details" onPress={() => {
+                props.navigation.navigate({
+                    routeName: 'MealDetail'
+                });
+            }}/>
         </View>
     )
 };
