5fe41a9052f345ad2b7cdaf5eda8956a17be3eb7
xiah <xiah0526@gmail.com>
2020-03-20 17:29:13 +0900

    udemy - 120. Setting Dynamic Navigation Options

--- the-meals-app/screens/CategoryMealScreen.js
+++ the-meals-app/screens/CategoryMealScreen.js
@@ -22,6 +22,16 @@ const CategoryMealScreen = props => {
     )
 };
 
+CategoryMealScreen.navigationOptions = navigationData => {
+    const catId = navigationData.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+    return {
+      headerTitle: selectedCategory.title
+    };
+};
+
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
