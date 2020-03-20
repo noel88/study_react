81450e1be7286fb076ff87aba57a2c3f633c6413
xiah <xiah0526@gmail.com>
2020-03-20 11:28:15 +0900

    udemy - 101. Using Platform-specific Code Files

--- rn-complete-guide1/components/MainButton.ios.js
+++ rn-complete-guide1/components/MainButton.ios.js
@@ -3,27 +3,18 @@ import {
     View,
     Text,
     StyleSheet,
-    TouchableOpacity,
-    Platform,
-    TouchableNativeFeedback
+    TouchableOpacity
 } from 'react-native';
 import Colors from "../constants/colors";
 
 const MainButton = props => {
-    let ButtonComponent = TouchableOpacity;
-
-    if (Platform.OS === 'android' && Platform.Version >= 21) {
-        ButtonComponent = TouchableNativeFeedback;
-    }
 
     return (
-        <View style={styles.buttonContainer}>
-            <ButtonComponent activeOpacity={0.8} onPress={props.onPress}>
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>{props.children}</Text>
-                </View>
-            </ButtonComponent>
-        </View>
+        <TouchableOpacity activeOpacity={0.6} onPress={props.onPress}>
+            <View style={styles.button}>
+                <Text style={styles.buttonText}>{props.children}</Text>
+            </View>
+        </TouchableOpacity>
     )
 };
 
