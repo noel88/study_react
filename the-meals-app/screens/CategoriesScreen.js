5b7c57273377485cbe8cb97a733ec4d4a95c51ff
xiah <xiah0526@gmail.com>
2020-03-21 16:31:25 +0900

    udemy - 122. Grid Styling & Some Refactoring

--- the-meals-app/screens/CategoriesScreen.js
+++ the-meals-app/screens/CategoriesScreen.js
@@ -9,24 +9,25 @@ import {
 } from 'react-native';
 import { CATEGORIES } from "../data/dummy-data";
 import Colors from "../constants/Colors";
+import CategoryGirdTile from "../components/CategoryGridTile";
+
+
 
 const CategoriesScreen = props => {
     const renderGridItem = (itemData) => {
         return (
-            <TouchableOpacity
-                style={styles.gridItem}
-                onPress={() => {
+            <CategoryGirdTile
+                title={itemData.item.title}
+                color={itemData.item.color}
+                onSelect={() => {
                     props.navigation.navigate({
                         routeName: 'CategoryMeals',
                         params: {
                             categoryId: itemData.item.id
                         }
                     });
-            }}>
-                <View>
-                    <Text>{itemData.item.title}</Text>
-                </View>
-            </TouchableOpacity>
+                }}
+            />
         )
     };
     return (
@@ -48,11 +49,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    },
-    gridItem: {
-        flex: 1,
-        margin: 15,
-        height: 150
     }
 });
 
