bd94ab712de492b1e0f90bc9c604ef804ca1ce8e
xiah <xiah0526@gmail.com>
2020-03-23 14:06:10 +0900

    udemy - 137. Adding a DefaultText Component

---
+++ the-meals-app/components/DefaultText.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
+    TouchableOpacity,
+    Platform,
+    TouchableNativeFeedback
+} from 'react-native';
+
+
+const DefaultText = props => {
+    return (
+        <Text style={styles.text}>{props.children}</Text>
+    )
+};
+
+const styles = StyleSheet.create({
+    text: {
+        fontFamily: 'open-sans'
+    }
+});
+
+export default DefaultText;
