689009f78987f48132724867cff7a3edb47085d3
xiah <xiah0526@gmail.com>
2020-03-24 08:51:13 +0900

    udemy (react) - bug fix

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/person';
 
-const App = () => {
-  this.state = {
+class App extends Component{
+  state = {
     persons: [
         {name: 'max', age: '28'},
         {name: 'Tony', age: '22'},
         {name: 'manu', age: '27'}
     ]
   };
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello, React app</h1>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name}  age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name}  age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+                <Person name={this.state.persons[2].name}  age={this.state.persons[2].age}/>
 
-  return (
-    <div className="App">
-      <h1>Hello, React app</h1>
-        <button>Switch Name</button>
-      <Person name={this.state.persons[0].name}  age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name}  age={this.state.persons[1].age}>>My Hobbies: Racing</Person>
-      <Person name={this.state.persons[2].name}  age={this.state.persons[2].age}/>
 
-
-    </div>
-  );
-}
+            </div>
+        );
+    }
+};
 
 export default App;
