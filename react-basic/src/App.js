166083693ffc302e244f84682f2a8234d7cec169
xiah <xiah0526@gmail.com>
2020-03-24 07:38:03 +0900

    udemy (react) - 37. Understanding the "children" Prop

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <h1>Hello, React app</h1>
       <Person name="jenny" age="20"/>
-      <Person name="manu" age="28"/>
+        <Person name="manu" age="28">My Hobbies: Racing</Person>
       <Person name="tony" age="27"/>
     </div>
   );
--- react-basic/src/Person/person.js
+++ react-basic/src/Person/person.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const person = (props) => {
     return (
-        <p> I'm {props.name} I'm {props.age} years old</p>
+        <div>
+            <p> I'm {props.name} I'm {props.age} years old.</p>
+            <p>{props.children}</p>
+        </div>
     )
 };
 
