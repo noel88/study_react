e69914cdd032487b2ba85b055696846cf5c02732
xiah <xiah0526@gmail.com>
2020-03-24 10:14:26 +0900

    udemy (react) - 53. Rendering Content Conditionally

--- react-basic/src/App.js
+++ react-basic/src/App.js
@@ -10,7 +10,8 @@ const App = props => {
             {name: 'mini', age : 20},
             {name: 'manu', age : 22}
         ],
-        otherState: 'some other value'
+        otherState: 'some other value',
+        showPersons: true
     });
 
     const switchNameHandler = (newName) => {
@@ -33,6 +34,12 @@ const App = props => {
         });
     };
 
+    const togglePersonsHandler = () => {
+        const doesShow = personsSates.showPersons;
+        setPersonsSates({showPersons: !doesShow});
+
+    };
+
     const style = {
         backgroundColor: 'white',
         font: 'inherit',
@@ -45,29 +52,31 @@ const App = props => {
         <div className="App">
             <h1>Hello, React app</h1>
             <button
-                onClick={() => switchNameHandler('new')}
+                onClick={togglePersonsHandler}
                 style={style}
             >Switch Name
             </button>
             {/*<button onClick={this.switchNameHandler.bind(this, 'new')}>Switch Name</button>*/}
 
-            <Person
-                name={personsSates.persons[0].name}
-                age={personsSates.persons[0].age}
-                changed={nameChangeHandler}
-            />
-            <Person
-                name={personsSates.persons[1].name}
-                age={personsSates.persons[1].age}
-                click={() => switchNameHandler('new')}
-            >My Hobbies: Racing
-            </Person>
-            <Person
-                name={personsSates.persons[2].name}
-                age={personsSates.persons[2].age}
-            />
-
-
+            { personsSates.showPersons === true ?
+                <div>
+                    <Person
+                        name={personsSates.persons[0].name}
+                        age={personsSates.persons[0].age}
+                        changed={nameChangeHandler}
+                    />
+                    <Person
+                        name={personsSates.persons[1].name}
+                        age={personsSates.persons[1].age}
+                        click={() => switchNameHandler('new')}
+                    >My Hobbies: Racing
+                    </Person>
+                    <Person
+                        name={personsSates.persons[2].name}
+                        age={personsSates.persons[2].age}
+                    />
+                </div> : null
+            }
         </div>
     );
 };
