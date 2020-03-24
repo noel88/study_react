b1c318a79e6b31e79e3f425fafd0c6cd2c262068
xiah <xiah0526@gmail.com>
2020-03-24 16:13:37 +0900

    udemy (react) - 136. Adding the Order Button

--- burger-builder/src/components/Burger/BuildControls/BuildControls.js
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -13,6 +13,47 @@ const BuildControls = styled.div`
   padding: 10px 0;
 `;
 
+const OrderButton = styled.button`
+    background-color: #DAD735;
+    outline: none;
+    cursor: pointer;
+    border: 1px solid #966909;
+    color: #966909;
+    font-family: inherit;
+    font-size: 1.2em;
+    padding: 15px 30px;
+    box-shadow: 2px 2px 2px #966909;
+    
+    &&:hover, &&:active {
+        background-color: #A0DB41;
+        border: 1px solid #966909;
+        color: #966909;
+    }
+    
+    &&:disabled {
+        background-color: #C7C6C6;
+        cursor: not-allowed;
+        border: 1px solid #ccc;
+        color: #888888;
+    }
+    
+    &&:not(:disabled) {
+      animation: enable 0.3s linear;
+    }
+    
+    @keyframes enable {
+        0% {
+            transform: scale(1);
+        }
+        60% {
+            transform: scale(1.1);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+`;
+
 const controls = [
     {label: 'Salad', type: 'salad'},
     {label: 'Bacon', type: 'bacon'},
@@ -34,6 +75,7 @@ const buildControls = (props) => {
                     disabled={props.disabled[ctrl.type]}
                 />
             ))}
+            <OrderButton disabled={!props.purchasable}>ORDER NOW</OrderButton>
         </BuildControls>
     )
 };
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,8 +18,20 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchasable: false
     };
+
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey]
+            }).reduce((sum, el) => {
+                return sum + el;
+            }, 0);
+        this.setState({purchasable: sum > 0})
+    }
+
 
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
@@ -32,6 +44,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice + priceAddition;
         this.setState({totalPrice: newPrice, ingredients: updateIngredients});
+        this.updatePurchaseState(updateIngredients);
 
     };
 
@@ -49,6 +62,7 @@ class BurgerBuilder extends Component {
         const oldPrice = this.state.totalPrice;
         const newPrice = oldPrice - priceDeduction;
         this.setState({totalPrice: newPrice, ingredients: updateIngredients});
+        this.updatePurchaseState(updateIngredients);
     };
 
     render() {
@@ -67,6 +81,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientremoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    purchasable={this.state.purchasable}
                     price={this.state.totalPrice}
                 />
             </Aux>
