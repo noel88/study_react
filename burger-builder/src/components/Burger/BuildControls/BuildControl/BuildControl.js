33879ede6de544b91017fe830b23dafa36eb5319
xiah <xiah0526@gmail.com>
2020-03-24 15:16:30 +0900

    udemy (react) - 131. Adding the Build Control Component

---
+++ burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,81 @@
+import React from "react";
+import styled from "styled-components";
+
+const BuildControl = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 5px 0;
+`;
+
+const Button = styled.button`
+  display: block;
+  font: inherit;
+  padding: 5px;
+  margin: 0 5px;
+  width: 80px;
+  border: 1px solid #aa6817;
+  cursor: pointer;
+  outline: none;
+  
+  &&:disabled {
+    background-color: #AC9980;
+    border: 1px solid #7E7365;
+    color: #ccc;
+    cursor: default;
+  }
+  
+  &&:hover:disabled {
+    background-color: #AC9980;
+    color: #ccc;
+    cursor: not-allowed;
+  }
+`;
+
+const Label = styled.div`
+    padding: 10px;
+    font-weight: bold;
+    width: 80px;
+`;
+
+const Less = styled.button`
+    background-color: #D39952;
+    color: white;
+    
+    &&:hover {
+      background-color: #DAA972;
+      color: white;
+    }
+    
+    &&:active {
+      background-color: #DAA972;
+      color: white;
+    }
+`;
+const More = styled.button`
+    background-color: #8F5E1E;
+    color: white;
+    
+    &&:hover {
+      background-color: #99703F;
+      color: white;
+    }
+    
+    &&:active {
+      background-color: #99703F;
+      color: white;
+    }
+`;
+
+const buildControl = (props) => {
+    return (
+        <BuildControl>
+            <Label>{props.label}</Label>
+            <Less>Less</Less>
+            <More>More</More>
+        </BuildControl>
+    )
+};
+
+
+export default buildControl;
---
+++ burger-builder/src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+const BuildControls = styled.div`
+  width: 100%;
+  background-color: #cf8f2e;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  box-shadow: 0 2px 1px #ccc;
+  margin: auto;
+  padding: 10px 0;
+`;
+
+
+
+const buildControls = (props) => {
+
+    return (
+        <BuildControls>
+
+        </BuildControls>
+    )
+};
+
+export default buildControls;
