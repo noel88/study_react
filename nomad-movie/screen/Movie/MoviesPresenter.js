4c28727661c92efef59ce51bf1a55cf2d0ec95eb
xiah <xiah0526@gmail.com>
2020-03-25 17:37:07 +0900

    nomad (react native) - #5.0 Making a Slider part One

--- nomad-movie/screen/Movie/MoviesPresenter.js
+++ nomad-movie/screen/Movie/MoviesPresenter.js
@@ -2,10 +2,21 @@ import React from "react";
 import { Text } from "react-native";
 import PropTypes from 'prop-types';
 import Loader from "../../components/Loader";
+import styled from "styled-components";
+import MovieSlider from "../../components/MovieSlider";
 
+const Container = styled.ScrollView`
+
+`;
 
 const MoviesPresenter = ({loading, upcoming, popular, nowPlaying}) => (
-       loading? <Loader /> : <Text>Movies</Text>
+       loading
+           ? ( <Loader /> )
+           : (
+               <Container>
+                   <MovieSlider movies={nowPlaying} />
+               </Container>
+             )
 );
 
 MoviesPresenter.propTypes = {
