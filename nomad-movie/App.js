2e52a74a43d701ee989611a14d094346ec7df68a
xiah <xiah0526@gmail.com>
2020-03-25 14:27:56 +0900

    nomad (react native) - #2.1 Custom Icons for the Tab Navigation

--- nomad-movie/App.js
+++ nomad-movie/App.js
@@ -35,11 +35,3 @@ export default class App extends Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- nomad-movie/navigation/TabNavigation.js
+++ nomad-movie/navigation/TabNavigation.js
@@ -1,15 +1,41 @@
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import React from "react";
+import {
+    Platform
+} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import MoviesScreen from "../screen/Movies";
 import TVScreen from "../screen/TV";
 import SearchScreen from "../screen/Search";
 import { BG_COLOR } from "../constants/Colors";
+import TabBarIcon from "../components/TabBarIcon";
 
 const TabNavigation = createBottomTabNavigator(
     {
-        Movies: MoviesScreen,
-        TV: TVScreen,
-        Search: SearchScreen
+        Movie: {
+         screen: MoviesScreen,
+         navigationOptions: {
+             tabBarIcon : ({focused}) => (
+                 <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-film' : 'md-film'} />
+             )
+         }
+        },
+        TV: {
+            screen: TVScreen,
+            navigationOptions: {
+                tabBarIcon : ({focused}) => (
+                    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-tv' : 'md-tv'} />
+                )
+            }
+        },
+        Search: {
+            screen: SearchScreen,
+            navigationOptions: {
+                tabBarIcon : ({focused}) => (
+                    <TabBarIcon focused={focused} name={Platform.OS === 'ios' ? 'ios-search' : 'md-search'} />
+                )
+            }
+        }
     },{
         tabBarOptions: {
             showLabel: false,
