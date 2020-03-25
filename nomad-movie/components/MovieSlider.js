a99acd23ebc8f3a3a5413e0015f1620938e83970
xiah <xiah0526@gmail.com>
2020-03-25 17:54:55 +0900

    nomad (react native) - #5.1 Making a Slider part Two - 1

--- nomad-movie/components/MovieSlider.js
+++ nomad-movie/components/MovieSlider.js
@@ -1,36 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from "styled-components";
-import Swiper from 'react-native-swiper';
+import Swiper from 'react-native-viewpager';
 import Layout from "../constants/Layout";
+import MovieSlide from "./MovieSlide";
 
 
 const SWIPER_HEIGHT = Layout.height / 3;
 
 const View = styled.View`
   background-color: red;
+  height: ${SWIPER_HEIGHT};
 `;
 const Text = styled.Text``;
 
 
 
 const MovieSlider = ({movies}) => (
-    <Swiper showPagination={false} autoplay={true} style={{ height: SWIPER_HEIGHT }}>
-        <View>
-            <Text>
-                First
-            </Text>
-        </View>
-        <View>
-            <Text>
-                Second
-            </Text>
-        </View>
-        <View>
-            <Text>
-                Third
-            </Text>
-        </View>
+    <Swiper showPagination={false} autoplay={true} style={{height: SWIPER_HEIGHT}}>
+        {movies ? movies.filter(movie => movie.backdrop_path !== null).map(movie => (
+            <View key={movie.id}>
+                <MovieSlide id={movie.id} posterPhoto={movie.poster_path} backgroundPhoto={movie.backdrop_path} title={movie.title} voteAvg={movie.vote_average} overview={movie.overview}/>
+            </View>
+        )) : null}
     </Swiper>
 );
 
