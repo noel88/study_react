00a20f9c25757a4bdd3bec4fa80fd8a2d3df3564
xiah <xiah0526@gmail.com>
2020-03-27 17:41:57 +0900

    udemy - 164. Working on the Product Details Screen

--- the-shop-app/screens/shop/ProductDetailsScreen.js
+++ the-shop-app/screens/shop/ProductDetailsScreen.js
@@ -8,6 +8,7 @@ import {
     ScrollView
 } from 'react-native';
 import { useSelector } from 'react-redux';
+import Colors from "../../constants/Colors";
 
 const ProductDetailScreen = props => {
     const productId = props.navigation.getParam('productId');
@@ -16,12 +17,39 @@ const ProductDetailScreen = props => {
 
 
     return (
-        <View>
-            <Text>{selectedProduct.title}</Text>
-        </View>
+        <ScrollView>
+            <Image style={styles.image} source={{uri: selectedProduct.imageUrl}} />
+            <View style={styles.actions}>
+                <Button color={Colors.primary} title="Add to Cart" onPress={() => {
+
+                }}/>
+            </View>
+            <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+            <Text style={styles.description}>{selectedProduct.description}</Text>
+        </ScrollView>
     )
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 300
+    },
+    price: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 20
+    },
+    description: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginHorizontal: 20
+    },
+    actions: {
+        marginVertical: 10,
+        alignItems: 'center'
+    }
+});
 
 export default ProductDetailScreen;
