55f624e66b18dbe2fb020d9eb0be03647b9126e7
xiah <xiah0526@gmail.com>
2020-09-04 17:42:28 +0900

    Setting 항목 정하고 대략적인 UI layout을 구현한다

--- DailyCommentMemo/app/screen/setting.js
+++ DailyCommentMemo/app/screen/setting.js
@@ -7,9 +7,17 @@ import {
   ScrollView,
   TextInput,
   TouchableOpacity,
+  Button,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
+import ToggleSwitch from 'toggle-switch-react-native';
+
+//TODO: Theme 설정
+//Description: Light Mode, Dark Mode
+
+//TODO: 데이터 모두 삭제하기
+//Description: AsyncStorage All Delete
 
 class Setting extends Component {
   constructor(props) {
@@ -18,7 +26,25 @@ class Setting extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Discover</Text>
+        <Text style={styles.title}>Setting</Text>
+        <View style={styles.settingView}>
+          <Text style={styles.subTitle}>Theme 변경</Text>
+          <ToggleSwitch
+            isOn={false}
+            onColor="green"
+            offColor="gray"
+            label="테마를 변경할 수 있습니다."
+            labelStyle={{color: 'black', fontWeight: '900'}}
+            size="large"
+            onToggle={(isOn) => console.log('changed to : ', isOn)}
+          />
+        </View>
+        <View style={styles.settingView}>
+          <View style={styles.row}>
+            <Text style={styles.subTitle}>데이터 전체를 삭제합니다.</Text>
+            <Button title={'삭제'} onPress={() => console.log('전체 삭제')} />
+          </View>
+        </View>
       </View>
     );
   }
@@ -29,8 +55,28 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  settingView: {
+    backgroundColor: '#fff',
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: '#000',
+    padding: 10,
+    margin: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end',
+  },
   title: {
     fontSize: 22,
+    fontWeight: 'bold',
+    color: '#000',
+    margin: 10,
+    marginLeft: 15,
+  },
+  subTitle: {
+    fontSize: 18,
     fontWeight: 'bold',
     color: '#000',
     margin: 10,
