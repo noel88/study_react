da47dd938da04f3f346a4a0e0d0128275a29b52a
Xiah <xiah0526@gmail.com>
2022-03-16 17:40:56 +0900

    React-typescript의 미니 프로젝트를 진행한다 (Todo-list) - 2

--- todo-list-for-typescript/src/ToDoList.tsx
+++ todo-list-for-typescript/src/ToDoList.tsx
@@ -21,23 +21,67 @@ import {useForm} from "react-hook-form";
 //   );
 // }
 
+interface IForm {
+  email: string;
+  firstname?: string;
+  lastname?: string;
+  username: string;
+  password: string;
+  password1: string;
+  extraError?: string;
+}
+
 function ToDoList() {
-  const { register, watch, handleSubmit, formState } = useForm();
-  const onValid = (data: any) => {
-    console.log(data)
+  const { register, watch, handleSubmit, formState: {errors}, setError } = useForm<IForm>({
+    defaultValues: {
+      email: "@gmail.com"
+    }
+  });
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError("password1", {message: "password are not the same."}, {shouldFocus: true})
+    }
+    // setError("extraError", {message: "Server offline"})
   }
+  console.log(errors)
   return (
     <div>
       <form
         style={{ display: "flex", flexDirection: "column"}}
         onSubmit={handleSubmit(onValid)}>
-        <input {...register("email", {required: true})} placeholder="Email"/>
-        <input {...register("first Name")} placeholder="first Name"/>
-        <input {...register("last Name")} placeholder="last Name"/>
-        <input {...register("username", {required: true, minLength: {value: 5, message: "Your username is too short"}})} placeholder="Username"/>
-        <input {...register("password", {required: true})} placeholder="password"/>
-        <input {...register("password 1", {required: "Password is required"})} placeholder="password1"/>
+        <input {...register("email", {
+          required: "email required",
+          pattern: {
+           value: /^[A-Za-z0-9._%+-]+@gmail.com/,
+           message: "Only Gmail emails allowed."
+          }
+        })} placeholder="Email"/>
+        <span>
+          {errors?.email?.message}
+        </span>
+        <input {...register("firstname", {
+          validate: {
+            noNico: (value) => value?.includes("nico") ? "no nicos allowed" : true,
+            noNick: (value) => value?.includes("nick") ? "no nicks allowed" : true,
+          }
+        })} placeholder="first Name"/>
+        <input {...register("lastname")} placeholder="last Name"/>
+        <input {...register("username", {required: "username is required", minLength: {value: 5, message: "Your username is too short"}})} placeholder="Username"/>
+        <span>
+          {errors?.username?.message}
+        </span>
+        <input {...register("password", {required: "Password is required"})} placeholder="password"/>
+        <span>
+          {errors?.password?.message}
+        </span>
+        <input {...register("password1", {required: "Password1 is required"})} placeholder="password1"/>
+        <span>
+          {errors?.password1?.message}
+        </span>
         <button>Add</button>
+        <span>
+          {errors?.extraError?.message}
+        </span>
       </form>
     </div>
   );
